3bd335ce1dac41a102d62555ac1a8b86290c9d01
dhruvin <dhruvinbhuva777@gmail.com>
2022-09-04 16:24:14 +0530

    home page design started(features section + navbar is done)

--- src/Pages/Chat/SingleChat/SingleMediaSection/SingleMediaSection.js
+++ src/Pages/Chat/SingleChat/SingleMediaSection/SingleMediaSection.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import CryptoJS from "crypto-js";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import smi1 from "Assets/single-media-i-1.png";
 import spm1 from "Assets/single-pdf-media.png";
 import starReveived from "Assets/star-received.svg";
@@ -8,12 +8,10 @@ import starSent from "Assets/star-sent.svg";
 import "./SingleMediaSection.css";
 
 function SingleMediaSection() {
-
   const state = useSelector((state) => state.SingleChatReducer);
   var { StaredMessages, imagesArray, documentsArray } = state;
 
-
-  useEffect(() => {}, []);
+  const [showAllImages, setShowAllImages] = useState(false);
 
   const getmonth = (month) => {
     switch (month) {
@@ -46,17 +44,15 @@ function SingleMediaSection() {
     }
   };
 
-
-
   const downloadDocumentToLocal = (documenturl) => {
-    let url = `${process.env.REACT_APP_AWS_BUCKET_PATH}${encodeURIComponent(documenturl)}`;
+    let url = `${process.env.REACT_APP_AWS_BUCKET_PATH}${encodeURIComponent(
+      documenturl
+    )}`;
 
-    let link = document.createElement('a');
+    let link = document.createElement("a");
     link.href = url;
     link.click();
-    
-  }
-
+  };
 
   const getDecryptedMessage = (message) => {
     return CryptoJS.AES.decrypt(
@@ -77,38 +73,70 @@ function SingleMediaSection() {
               {imagesArray.length > 3 ? "See All" : ""}
             </div>
           </div>
-          <div className="single-media-content">
-              {imagesArray.length > 0 &&
-            <div className="single-media-file">
-              <div className="single-media-file-container">
-                <img src={`${process.env.REACT_APP_AWS_BUCKET_PATH}${encodeURIComponent(imagesArray[0].key)}`} alt=""></img>
-              </div>
-            </div>
-              }
-            {imagesArray.length > 1 &&
-            <div className="single-media-file">
-              <div className="single-media-file-container">
-                <img src={`${process.env.REACT_APP_AWS_BUCKET_PATH}${encodeURIComponent(imagesArray[1].key)}`} alt=""></img>
-              </div>
-            </div>
-            }
-
-            {imagesArray.length > 2 ? (
-              <>
-                <div className="single-media-file">
-                  <div className="single-media-file-container single-media-info">
-                    {imagesArray.length - 2}+
+          {!showAllImages ? (
+            <>
+              <div className="single-media-content">
+                {imagesArray.length > 0 && (
+                  <div className="single-media-file">
+                    <div className="single-media-file-container">
+                      <img
+                        src={`${
+                          process.env.REACT_APP_AWS_BUCKET_PATH
+                        }${encodeURIComponent(imagesArray[0].key)}`}
+                        alt=""
+                      ></img>
+                    </div>
                   </div>
-                </div>
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
+                )}
+                {imagesArray.length > 1 && (
+                  <div className="single-media-file">
+                    <div className="single-media-file-container">
+                      <img
+                        src={`${
+                          process.env.REACT_APP_AWS_BUCKET_PATH
+                        }${encodeURIComponent(imagesArray[1].key)}`}
+                        alt=""
+                      ></img>
+                    </div>
+                  </div>
+                )}
+
+                {imagesArray.length > 2 ? (
+                  <>
+                    <div className="single-media-file"  onClick={() => setShowAllImages(true)}>
+                      <div className="single-media-file-container single-media-info">
+                        {imagesArray.length - 2}+
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="single-media-content show-all-media" id="single-media-content">
+                {imagesArray.map((image) => {
+                  return (
+                    <>
+                      <div className="single-media-file">
+                        <div className="single-media-file-container">
+                          <img
+                            src={`${
+                              process.env.REACT_APP_AWS_BUCKET_PATH
+                            }${encodeURIComponent(image.key)}`}
+                            alt=""
+                          ></img>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </>
+          )}
         </div>
-
-
-
 
         <div className="single-files-section">
           <div className="single-media-header fixed">
@@ -118,12 +146,14 @@ function SingleMediaSection() {
             <div className="single-media-see">See All</div>
           </div>
 
-
           {documentsArray.map((document) => {
             return (
               <>
                 <div className="single-files-container">
-                  <div className="single-file-icon" onClick={()=>downloadDocumentToLocal(document.key)}>
+                  <div
+                    className="single-file-icon"
+                    onClick={() => downloadDocumentToLocal(document.key)}
+                  >
                     <img src={spm1} alt=""></img>
                   </div>
                   <div className="single-file-details">
@@ -141,12 +171,7 @@ function SingleMediaSection() {
               </>
             );
           })}
-
-
         </div>
-
-
-
 
         <div className="single-star-section">
           <div className="single-media-header">
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,84 +1,91 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import GroupIcon from "Assets/Group.png";
-import SecurityIcon from "Assets/Security.png";
-import VideoIcon from "Assets/VideoCall.png";
-import FileIcon from "Assets/FileSharing.png";
-import MsgIcon from "Assets/MessageIcon.png";
+import GroupIcon from "Assets/group-chat.gif";
+import SecurityIcon from "Assets/946-equity-security-gradient.gif";
+import Videogif from "Assets/970-video-conference-gradient.gif";
+import FileGif from "Assets/56-document-gradient.gif";
+import Msggif from "Assets/177-envelope-mail-send-gradient.gif";
 import Logo from "Assets/Logo.png";
 import "./Home.css";
-
 
 function Home() {
   let navigate = useNavigate();
   return (
-    <div className='landingPageContainer'>
-      <div className='home-navigation'>
-        <ul className='logoList'>
-          <li>
-            <img src={Logo} alt="chitchat logo" />
-          </li>
-        </ul>
-
-        <ul className='navList'>
-          <li onClick={()=>{ navigate('/login')}}>Login</li>
-          <li>About Us</li>
-          <li>Support</li>
-          <li>Blog</li>
-        </ul>
-      </div>
-      <div className='mainContainer'>
-        <h1>Talking with everyone and keep secure</h1>
-        <button onClick={()=>{ navigate('/signup')}}>Get Started</button>
-        <div className='feauturesContainer'>
-          <div className='feauture'>
-            <img src={MsgIcon} alt="message icon" />
-            <h3>Messaging</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </p>
-          </div>
-
-          <div className='feauture'>
-            <img src={GroupIcon} alt="message icon" />
-            <h3>Group Chat</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </p>
-          </div>
-
-          <div className='feauture'>
-            <img src={VideoIcon} alt="message icon" />
-            <h3>Video Call</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </p>
-          </div>
-
-          <div className='feauture'>
-            <img src={FileIcon} alt="message icon" />
-            <h3>File Sharing</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </p>
-          </div>
-
-          <div className='feauture'>
-            <img src={SecurityIcon} alt="message icon" />
-            <h3>Privacy & Security</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </p>
-          </div>
+    <>
+    <header className="header">
+      <navbar className="navbar">
+        <div className="home-logo"><img src={Logo} alt='chit-chat'></img></div>
+        <div className="nav-links">
+          <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Signup</li>
+            <li>Blog</li>
+          </ul>
         </div>
+      </navbar>
+
+      <div className="header-desc">
+        
       </div>
-    </div>
-  )
+    </header>
+
+
+    <section className="features">
+      <div className="container-home">
+      <h1 className="feature-header">Check out All Features</h1>
+
+      <div className="first-row">
+
+        <container className="feature-container">
+          <div className="feature-card">
+            <img className="feature-logo" src={Msggif} alt=""></img>
+            <p className="feature-name">Messaging</p>
+            <p className="feature-description">Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+          </div>
+        </container>
+
+        <container className="feature-container">
+          <div className="feature-card">
+            <img className="feature-logo" src={GroupIcon}  alt=""></img>
+            <p className="feature-name">Group Chat</p>
+            <p className="feature-description">Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+          </div>
+        </container>
+
+        <container className="feature-container">
+          <div className="feature-card">
+            <img className="feature-logo" src={Videogif} alt=""></img>
+            <p className="feature-name">Video Call</p>
+            <p className="feature-description">Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+          </div>
+        </container>
+
+      </div>
+
+      <div className="second-row">
+
+        <container className="feature-container">
+          <div className="feature-card">
+            <img className="feature-logo" src={FileGif} alt=""></img>
+            <p className="feature-name">File Sharing</p>
+            <p className="feature-description">Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+          </div>
+        </container>
+
+        <container className="feature-container">
+          <div className="feature-card">
+            <img className="feature-logo" src={SecurityIcon} alt=""></img>
+            <p className="feature-name">Privacy & Security</p>
+            <p className="feature-description">Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
+          </div>
+        </container>
+
+      </div>
+      </div>
+    </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
